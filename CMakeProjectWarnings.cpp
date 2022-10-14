7311b3483d8247278103a8cf33c3317672856863
Michael Goulding <Michael.Goulding@microsoft.com>
2022-10-14 12:07:34 -0700

    Add project files.

---
+++ CMakeProjectWarnings.cpp
@@ -0,0 +1,14 @@
+﻿// CMakeProjectWarnings.cpp : Defines the entry point for the application.
+//
+
+#include "CMakeProjectWarnings.h"
+
+#include <atomic>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
